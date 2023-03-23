778e6d7f80b833f2b25db9f8a7144e19c49f3305
It's Najmul <111493279+N41M01@users.noreply.github.com>
2023-03-23 10:56:46 +0600

    Create sticker.sql

---
+++ .config/mpd/sticker.sql
@@ -0,0 +1,6 @@
+SQLite format 3@  .K�
+
+j
+^'�indexsticker_valuestickerCREATE UNIQUE INDEX sticker_value ON sticker(type, uri, name)��}tablestickerstickerCREATE TABLE sticker(  type VARCHAR NOT NULL,   uri VARCHAR NOT NULL,   name VARCHAR NOT NULL,   value VARCHAR NOT NULL)
+
+
